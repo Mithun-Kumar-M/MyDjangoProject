e402bbf715d69ec709d31160a13d74eab85d2b0f
Mithun Kumar <mithun.kumar@threemsolutions.com>
2025-05-14 19:12:28 +0530

    changes done

--- text_utils/text_utils/urls.py
+++ text_utils/text_utils/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
-    path('about',views.about,name='about')
+    path('',views.index,name='index')
+    
 ]
--- text_utils/text_utils/views.py
+++ text_utils/text_utils/views.py
@@ -2,8 +2,8 @@
 
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('''<a href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F">Facebook</a>''')
+from django.shortcuts import render 
 
-def about(request):
-    return HttpResponse('''<a href='https://www.instagram.com/#'>instagram</a>''')
+def index(request):
+    params = {'name':'Mithun','age':27}
+    return render(request,'index.html',params)
